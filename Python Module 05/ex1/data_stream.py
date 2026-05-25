8e613ab90884ee8d3d332c708a496f661330d104
lamrung07 <lamrung012345@gmail.com>
2026-05-25 21:26:16 +0200

    23/5

--- Python Module 05/ex1/data_stream.py
+++ Python Module 05/ex1/data_stream.py
@@ -86,15 +86,45 @@ class LogProcessor(DataProcessor):
             
 class DataStream():
     def __init__(self):
-        self.manager: dict[str, typing.Any] = {}
+        self.processors: list[DataProcessor] = []
     def register_processor(self, proc: DataProcessor) -> None:
-        pass
+        self.processors.append(proc)
     def process_stream(self, stream: list[typing.Any]) -> None:
-        pass
+        for element in stream:
+            validated = False
+            for proc in self.processors:
+                if proc.validate(element):
+                    proc.ingest(element)
+                    validated = True
+                    break
+            if not validated:
+                print(f"DataStream error - Can't process element in stream: {element}")
     def print_processors_stats(self) -> None:
-        pass
+        print("== DataStream statistics ==")
+        if not self._processors:
+            print("No processor found, no data")
+            return
+        for proc in self._processors:
+            print(proc.output())
     
 if __name__ == "__main__":
     print("=== Code Nexus - Data Stream ===\n")
     print("Initialize Data Stream...\n== DataStream statistics ==")
-    
+    stream = DataStream()
+    numeric_proc = NumericProcessor()
+    text_proc = TextProcessor()
+    log_proc = LogProcessor()
+    print("Registering Numeric Processor")
+    stream.register_processor(numeric_proc)
+    batch = [
+        "Hello world",
+        [3.14, -1, 2.71],
+        [
+            {"log_level": "WARNING", "log_message": "Telnet access! Use ssh instead"},
+            {"log_level": "INFO", "log_message": "User wil is connected"},
+        ],
+        42,
+        ["Hi", "five"],
+    ]
+    print(f"Send first batch of data on stream: {batch}")
+    stream.process_stream(batch)
