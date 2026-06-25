19633fdd8071f0bb1010b2dc701d378af4376549
lamrung07 <lamrung012345@gmail.com>
2026-06-25 08:40:15 +0200

    fix Module06

--- Python Module 07/ex0/__init__.py
+++ Python Module 07/ex0/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-from .ex0 import *
+from .ex0 import CreatureFactory, FlameFactory, AquaFactory
+
+# Declare as intentional re-exports
+__all__ = [
+    "CreatureFactory",
+    "FlameFactory",
+    "AquaFactory",
+]
